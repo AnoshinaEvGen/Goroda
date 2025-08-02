87f05a7be98d80a1c6f9d7e15f92778ca76adce8
AnoshinaEvGen <mega-feya@mail.ru>
2025-08-02 22:35:55 +0300

    Возвращаем результаты на русском

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # ключ ab15d101be244d1d96485ebc43787594
 
 from opencage.geocoder import OpenCageGeocode
+from pygments.lexers.sql import language_re
 
 
 def get_coordinates(city, key):
     try:
         geocoder = OpenCageGeocode(key)
-        query = city
-        results = geocoder.geocode(query)
+        results = geocoder.geocode(city, language= "ru")
         if results:
             return results[0]['geometry']['lat'], results[0]['geometry']['lng']
         else:
